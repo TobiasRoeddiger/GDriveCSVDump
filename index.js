b2895672d27b29bd6654dc480abc0aacbf265ea0
TobiasRoeddiger <usdkr@student.kit.edu>
2019-11-25 08:29:32 +0100

    basic implementation for accessing, reading and writing things

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const async = require('async');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = 'token.json';
 
+var drive;
+
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), listFiles);
+  authorize(JSON.parse(content), main);
 });
 
 /**
@@ -66,31 +70,86 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
-      });
+function folderExists(name)
+{
+  var pageToken = null;
+  // Using the NPM module 'async'
+  async.doWhilst(function (callback) {
+    drive.files.list({
+      q: "mimeType='application/vnd.google-apps.folder'",
+      fields: 'nextPageToken, files(id, name)',
+      spaces: 'drive',
+      pageToken: pageToken
+    }, function (err, res) {
+      if (err) {
+        // Handle error
+        console.error(err);
+        callback(err)
+      } else {
+        if (res.data.files !== undefined)
+        {
+          res.data.files.forEach(function (file) {
+            if (file.name === name)
+            {
+              console.log(file);
+            }
+          });
+        }
+        pageToken = res.nextPageToken;
+        callback();
+      }
+    });
+  }, function () {
+    return !!pageToken;
+  }, function (err) {
+    if (err) {
+      // Handle error
+      console.error(err);
     } else {
-      console.log('No files found.');
+      return 
+    }
+  })
+}
+
+function createRootFolder()
+{
+  if (!drive) return;
+
+  var fileMetadata = {
+    'name': 'photo.jpg'
+  };
+  var media = {
+    mimeType: 'image/jpeg',
+    body: fs.createReadStream('files/image.jpg')
+  };
+  drive.files.create({
+    resource: fileMetadata,
+    media: media,
+    fields: 'id'
+  }, function (err, file) {
+    if (err) {
+      // Handle error
+      console.error(err);
+    } else {
+      console.log('File Id: ', file.id);
     }
   });
+}
+
+/**
+ * Intializes all server functionalities.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function main(auth) {
+  drive = google.drive({version: 'v3', auth});
+  if(!folderExists('Sensor Data'))
+  {
+    createRootFolder();
+  }
 }
 // [END drive_quickstart]
 
 module.exports = {
   SCOPES,
-  listFiles,
+  main,
 };
